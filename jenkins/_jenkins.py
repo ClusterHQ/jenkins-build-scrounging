5aba48501ffe69dd96a0a58ca7a74c73cdc83dcf
Jonathan Lange <jml@mumak.net>
2016-01-06 17:23:43 +0000

    More code

--- jenkins/_jenkins.py
+++ jenkins/_jenkins.py
@@ -1,6 +1,7 @@
 # Copyright (c) ClusterHQ Ltd. See LICENSE for details.
 
 import collections
+import pandas
 
 
 SUCCESS = u'SUCCESS'
@@ -20,3 +21,23 @@ def get_build_result(build):
 
 def summarize_build_results(builds):
     return collections.Counter(map(get_build_result, builds))
+
+
+def _flatten_build(build):
+    for sub_build in build['subBuilds']:
+        yield {
+            'number': build['number'],
+            'sub_number': sub_build['buildNumber'],
+            'job': sub_build['jobName'],
+            'result': sub_build['result'],
+        }
+
+
+def _flatten_builds(builds):
+    for build in builds:
+        for thing in _flatten_build(build):
+            yield thing
+
+
+def make_data_frame(builds):
+    return pandas.DataFrame(_flatten_builds(builds))
