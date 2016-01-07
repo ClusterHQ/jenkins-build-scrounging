4ce0026154dc3c8ac4cecdc619f9e105fc9958ac
James Westby <jw+debian@jameswestby.net>
2016-01-07 09:58:10 +0000

    Convert to treq and concurrent downloads of logs.
    Also a start at log categorisation.

--- jenkins/_jenkins.py
+++ jenkins/_jenkins.py
@@ -8,21 +8,42 @@ SUCCESS = u'SUCCESS'
 FAILURE = u'FAILURE'
 
 
-import requests
+import treq
+from twisted.internet import defer
+from twisted.internet import reactor
+
 BASE_URL = 'http://ci-live.clusterhq.com:8080/'
 BASE_PATH = 'job/ClusterHQ-flocker/job/master/job/__main_multijob/'
+MAX_CONCURRENT_REQUESTS = 5
 
 
 def jenkins_get(path):
     cookies = {'JSESSIONID.f304e28f': 'YOURCOOKIEHERE'}
-    return requests.get(BASE_URL + path, cookies=cookies)
+    return treq.get(BASE_URL + path, cookies=cookies)
+
+
+class RequestFailed(Exception):
+
+    def __init__(self, response):
+        self.response = response
+
+    def __str__(self):
+        return "Request failed with code {}".format(self.response.code)
 
 
 def jenkins_json_get(path):
-    return jenkins_get(path).json()
+    def decode_json(resp):
+        if resp.code != 200:
+            raise RequestFailed(resp)
+        return resp.json()
+    return jenkins_get(path).addCallback(decode_json)
 
 
-builds = jenkins_json_get(BASE_PATH + 'api/json?tree=builds[result,number,subBuilds[result,buildNumber,jobName,url]]')['builds']
+def extract_builds(resp):
+    return resp['builds']
+
+d = jenkins_json_get(BASE_PATH + 'api/json?tree=builds[result,number,subBuilds[result,buildNumber,jobName,url]]')
+d.addCallback(extract_builds)
 
 
 def get_build_result(build):
@@ -70,37 +91,124 @@ def get_top_failing_jobs(build_data):
 
 
 def get_console_text(job_url):
-    return jenkins_get(url + '/consoleText').content
+    def content_for_200(resp):
+        if resp.code == 200:
+            return resp.content()
+        return defer.succeed(None)
+    return jenkins_get(job_url + '/consoleText').addCallback(content_for_200)
 
 
 # XXX: Use t.p.constants
 UNKNOWN = 0
 NULLPOINTEREXCEPTION = 1
+ARGPARSE130 = 2
+TIMEOUT = 3
+TESTTOOLS182 = 4
+SLAVEOFFLINE = 5
+DOCKER500 = 6
+REMOVEOBSERVER = 7
+MISSINGLOG = 8
+GITCLEAN = 9
+
+DESCRIPTIONS = {
+    UNKNOWN: "Unknown",
+    NULLPOINTEREXCEPTION: "[FLOC-3725] NullPointerException",
+    ARGPARSE130: "[FLOC-?] PyPI down",
+    TIMEOUT: "[FLOC-?] Build timeout",
+    TESTTOOLS182: "[FLOC-?] testtools==1.8.2chq1 unavailable",
+    SLAVEOFFLINE: "[FLOC-?] Slave went offline during the build",
+    DOCKER500: "[FLOC-3077] docker.errors.APIError: 500 Server Error",
+    REMOVEOBSERVER: "[FLOC-3681(fixed)] removeObserver on observer not in list",
+    MISSINGLOG: "Missing log",
+    GITCLEAN: "[FLOC-?] FATAL: Command 'git clean -fdx' returned status code 1",
+}
 
 
 def classify_build_log(log):
     if 'NullPointerException' in log:
-        return NULLPOINTEREXCEPTION
-    return UNKNOWN
+        return DESCRIPTIONS[NULLPOINTEREXCEPTION]
+    if 'No matching distribution found for argparse==1.3.0' in log:
+        return DESCRIPTIONS[ARGPARSE130]
+    if 'Build timed out' in log:
+        return DESCRIPTIONS[TIMEOUT]
+    if 'No matching distribution found for testtools==1.8.2chq1' in log:
+        return DESCRIPTIONS[TESTTOOLS182]
+    if 'Slave went offline during the build' in log:
+        return DESCRIPTIONS[SLAVEOFFLINE]
+    if 'error: flocker.node.functional.test_docker.NamespacedDockerClientTests.test_pull_image_if_necessary' in log and 'docker.errors.APIError: 500 Server Error: Internal Server Error ("Unknown device' in log:
+        return DESCRIPTIONS[DOCKER500]
+    if 'stderr:ValueError: list.remove(x): x not in list' in log:
+        return DESCRIPTIONS[REMOVEOBSERVER]
+    if 'FATAL: Command "git clean -fdx" returned status code 1:' in log:
+        return DESCRIPTIONS[GITCLEAN]
+    print "====="
+    print "Unknown failure reason:"
+    print "\n".join(log.splitlines()[-150:])
+    print ""
+    return DESCRIPTIONS[UNKNOWN]
 
 
-print "Top-level build results:"
-print summarize_build_results(builds)
-print ""
-print ""
-print "Jobs with the most failures"
-build_data = make_data_frame(builds)
-failing_jobs = get_top_failing_jobs(build_data)
-print failing_jobs.head(20)
+def print_summary_results(builds):
+    print "Top-level build results:"
+    print summarize_build_results(builds)
+    return builds
+
+d.addCallback(print_summary_results)
 
 
-top_job = failing_jobs.index[0]
+def print_top_failing_jobs(builds):
+    print ""
+    print ""
+    print "Jobs with the most failures"
+    build_data = make_data_frame(builds)
+    failing_jobs = get_top_failing_jobs(build_data)
+    print failing_jobs.head(20)
 
-individual_failures = build_data[build_data['result'] == FAILURE]
+    return build_data
 
-classifications = []
-for url in individual_failures['url']:
-    classifications.append(classify_build_log(get_console_text(url)))
+d.addCallback(print_top_failing_jobs)
 
-individual_failures['classification'] = pandas.Series(classifications, index=individual_failures.index)
-print individual_failures.groupby('classification').size().sort_values(ascending=False)
+
+def print_common_failure_reasons(build_data):
+    individual_failures = build_data[build_data['result'] == FAILURE]
+
+    deferreds = []
+    sem = defer.DeferredSemaphore(MAX_CONCURRENT_REQUESTS)
+    for url in individual_failures['url']:
+        d = sem.run(get_console_text, url)
+        deferreds.append(d)
+
+    dl = defer.DeferredList(deferreds)
+
+    def classify_results(results):
+        classifications = []
+        for success, log in results:
+            if success and log is not None:
+                classifications.append(classify_build_log(log))
+            else:
+                classifications.append(DESCRIPTIONS[MISSINGLOG])
+        return classifications
+
+    def print_classification(classifications):
+        individual_failures['classification'] = pandas.Series(classifications, index=individual_failures.index)
+        print individual_failures.groupby('classification').size().sort_values(ascending=False)
+
+    dl.addCallback(classify_results)
+    dl.addCallback(print_classification)
+    return dl
+
+
+d.addCallback(print_common_failure_reasons)
+
+def print_err(failure):
+    print failure
+    return failure
+
+d.addErrback(print_err)
+
+def cbShutdown(ignored):
+        reactor.stop()
+
+d.addBoth(cbShutdown)
+
+reactor.run()
